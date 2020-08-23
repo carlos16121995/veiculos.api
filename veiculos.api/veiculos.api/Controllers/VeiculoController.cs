a98015a8e34fb97e37ca5a94c3f39b56bb8a493c
Carlos Eduardo Silva de Alcântara <carloseduardo161219@gmail.com>
2020-08-22 23:35:40 -0300

    Actions GravarMarcaVeiculo, AlterarMarcaVeiculo, BuscarMarcaVeiculo, ExcluirMarcaVeiculo do VeiculoController Funções Gravar, Alterar, Buscar,Excluir do MarcaVeiculo

--- veiculos.api/veiculos.api/Controllers/VeiculoController.cs
+++ veiculos.api/veiculos.api/Controllers/VeiculoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using veiculos.api.Models;
 
 namespace veiculos.api.Controllers
 {
@@ -11,5 +12,73 @@ namespace veiculos.api.Controllers
     [ApiController]
     public class VeiculoController : Controller
     {
+
+        [HttpPost("[action]")]
+        public ActionResult GravarMarcaVeiculo(MarcaVeiculo marcaVeiculo)
+        {
+            if ((marcaVeiculo.Id == 0) && (marcaVeiculo.Nome != "") && (marcaVeiculo.Nome.Length > 2) && (marcaVeiculo.Nome.Length <= 30))
+            {
+                int ret = marcaVeiculo.Gravar();
+                if (ret > 0)
+                {
+                    return Ok("Cadastrado com sucesso!");
+                } 
+            }
+
+            return BadRequest();
+        }
+        [HttpPut("[action]")]
+        public ActionResult AlterarMarcaVeiculo(MarcaVeiculo marcaVeiculo)
+        {
+            if ((marcaVeiculo.Id > 0) && (marcaVeiculo.Nome != "") && (marcaVeiculo.Nome.Length > 2) && (marcaVeiculo.Nome.Length <= 30))
+            {
+                int ret = marcaVeiculo.Alterar();
+                if (ret > 0)
+                {
+                    return Ok("Alterado com sucesso!");
+                }
+            }
+
+            return BadRequest();
+        }
+        [HttpGet("[action]/{id}")]
+        public ActionResult BuscarMarcaVeiculo(int id)
+        {
+            MarcaVeiculo marcaVeiculo = new MarcaVeiculo().Buscar(id);
+
+            if (marcaVeiculo == null)
+            {
+                return NotFound();
+            }
+            return Json(marcaVeiculo);
+        }
+        [HttpGet("[action]")]
+        public ActionResult BuscarMarcaVeiculo()
+        {
+            List<MarcaVeiculo> marcaVeiculo = new MarcaVeiculo().Buscar();
+
+            if (marcaVeiculo == null)
+            {
+                return NotFound();
+            }
+            return Json(marcaVeiculo);
+        }
+        [HttpDelete]
+        public IActionResult ExcluirMarcaVeiculo(int id)
+        {
+
+            int ret = new MarcaVeiculo().Excluir(id);
+
+            if (ret == -99)
+            {
+                return Unauthorized("Esta marca possui um veiculo relacionado a ele. Remova o relacionamento antes de excluir.");
+            }
+            if (ret > 0)
+            {
+                return Ok("Excluido com sucesso!");
+            }
+            return BadRequest();
+
+        }
     }
 }
--- veiculos.api/veiculos.api/Models/MarcaVeiculo.cs
+++ veiculos.api/veiculos.api/Models/MarcaVeiculo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace veiculos.api.Models
 {
@@ -14,5 +15,89 @@ namespace veiculos.api.Models
         public string Nome { get; set; }
 
         public virtual ICollection<Veiculo> Veiculo { get; set; }
+
+
+        public int Gravar()
+        {
+            try
+            {
+                using (veiculosapiContext contexto = new veiculosapiContext())
+                {  
+                    contexto.MarcaVeiculo.Add(this);
+                    return contexto.SaveChanges(); 
+                }
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+        public int Alterar()
+        {
+            try
+            {
+                using (veiculosapiContext contexto = new veiculosapiContext())
+                {
+                    MarcaVeiculo marcaVeiculo = contexto.MarcaVeiculo.Where(p => p.Id == this.Id).FirstOrDefault();
+                    marcaVeiculo.Nome = this.Nome; 
+                    contexto.MarcaVeiculo.Attach(marcaVeiculo);
+                    return contexto.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+        public MarcaVeiculo Buscar(int id)
+        {
+            try
+            {
+                using (veiculosapiContext contexto = new veiculosapiContext())
+                {
+                    return contexto.MarcaVeiculo.Where(p => p.Id == id).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public List<MarcaVeiculo> Buscar()
+        {
+            try
+            {
+                using (veiculosapiContext contexto = new veiculosapiContext())
+                {
+                    return contexto.MarcaVeiculo.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public int Excluir(int id)
+        {
+            try
+            {
+                using (veiculosapiContext contexto = new veiculosapiContext())
+                {
+                    Veiculo veiculo = contexto.Veiculo.Where(p => p.MarcaVeiculoId == id).FirstOrDefault();
+                    if (veiculo == null)
+                    {
+                        MarcaVeiculo marcaVeiculo = contexto.MarcaVeiculo.Where(p => p.Id == id).FirstOrDefault();
+                        contexto.MarcaVeiculo.Remove(marcaVeiculo);
+                        return contexto.SaveChanges();
+                    }
+                    return -99;
+                }
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+
     }
 }
